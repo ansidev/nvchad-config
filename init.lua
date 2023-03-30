9549ab4066c8a2e12fd196c55f9cc57bd1863ffb
Le Minh Tri <ansidev@gmail.com>
2023-03-30 16:42:30 +0700

    chore: add comment

--- init.lua
+++ init.lua
@@ -1,8 +1,9 @@
 local autocmd = vim.api.nvim_create_autocmd
 
+-- auto format file on saving
 autocmd("BufWritePre", {
   pattern = "*",
-  command = "lua vim.lsp.buf.format({ async = true })"
+  command = "lua vim.lsp.buf.format({ async = true })",
 })
 
 -- Auto resize panes when resizing nvim window
